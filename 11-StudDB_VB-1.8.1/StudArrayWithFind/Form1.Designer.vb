042c66888d282a8e07e8e78508d2d3b745217f59
Hayden-666 <62979549+Hayden-666@users.noreply.github.com>
2020-11-30 23:19:22 +1100

    Add files via upload

---
+++ 11-StudDB_VB-1.8.1/StudArrayWithFind/Form1.Designer.vb
@@ -0,0 +1,268 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtGender = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtAvMk = New System.Windows.Forms.TextBox()
+        Me.btnAddStud = New System.Windows.Forms.Button()
+        Me.lstStud = New System.Windows.Forms.ListBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.chkPaid = New System.Windows.Forms.CheckBox()
+        Me.txtPhone = New System.Windows.Forms.MaskedTextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtDOB = New System.Windows.Forms.DateTimePicker()
+        Me.btnFindStud = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(72, 160)
+        Me.txtFirstName.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(196, 31)
+        Me.txtFirstName.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(72, 67)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(215, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter Student Details"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(72, 123)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(116, 25)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "First Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(284, 123)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(115, 25)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Last Name"
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(284, 160)
+        Me.txtLastName.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(196, 31)
+        Me.txtLastName.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(496, 123)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(382, 25)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Date of Birth (you can type d/mm/yyyy)"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(76, 221)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(185, 25)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Gender (""m"" or ""f"")"
+        '
+        'txtGender
+        '
+        Me.txtGender.Location = New System.Drawing.Point(76, 258)
+        Me.txtGender.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.txtGender.Name = "txtGender"
+        Me.txtGender.Size = New System.Drawing.Size(196, 31)
+        Me.txtGender.TabIndex = 3
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(288, 221)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(202, 25)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Average Mk (0-100)"
+        '
+        'txtAvMk
+        '
+        Me.txtAvMk.Location = New System.Drawing.Point(288, 258)
+        Me.txtAvMk.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.txtAvMk.Name = "txtAvMk"
+        Me.txtAvMk.Size = New System.Drawing.Size(196, 31)
+        Me.txtAvMk.TabIndex = 4
+        '
+        'btnAddStud
+        '
+        Me.btnAddStud.Location = New System.Drawing.Point(910, 250)
+        Me.btnAddStud.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.btnAddStud.Name = "btnAddStud"
+        Me.btnAddStud.Size = New System.Drawing.Size(208, 44)
+        Me.btnAddStud.TabIndex = 11
+        Me.btnAddStud.Text = "Add Student"
+        Me.btnAddStud.UseVisualStyleBackColor = True
+        '
+        'lstStud
+        '
+        Me.lstStud.FormattingEnabled = True
+        Me.lstStud.ItemHeight = 25
+        Me.lstStud.Location = New System.Drawing.Point(78, 383)
+        Me.lstStud.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.lstStud.Name = "lstStud"
+        Me.lstStud.Size = New System.Drawing.Size(1062, 304)
+        Me.lstStud.TabIndex = 15
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(78, 352)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(352, 25)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "List of entered students (for testing)"
+        '
+        'chkPaid
+        '
+        Me.chkPaid.AutoSize = True
+        Me.chkPaid.Location = New System.Drawing.Point(750, 262)
+        Me.chkPaid.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.chkPaid.Name = "chkPaid"
+        Me.chkPaid.Size = New System.Drawing.Size(87, 29)
+        Me.chkPaid.TabIndex = 6
+        Me.chkPaid.Text = "Paid"
+        Me.chkPaid.UseVisualStyleBackColor = True
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(506, 258)
+        Me.txtPhone.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.txtPhone.Mask = "0000-000-000"
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(196, 31)
+        Me.txtPhone.TabIndex = 5
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(502, 221)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(175, 25)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Phone (10 digits)"
+        '
+        'txtDOB
+        '
+        Me.txtDOB.Location = New System.Drawing.Point(498, 160)
+        Me.txtDOB.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.txtDOB.MaxDate = New Date(2004, 12, 31, 0, 0, 0, 0)
+        Me.txtDOB.MinDate = New Date(1998, 1, 1, 0, 0, 0, 0)
+        Me.txtDOB.Name = "txtDOB"
+        Me.txtDOB.Size = New System.Drawing.Size(406, 31)
+        Me.txtDOB.TabIndex = 2
+        Me.txtDOB.Value = New Date(2003, 1, 1, 0, 0, 0, 0)
+        '
+        'btnFindStud
+        '
+        Me.btnFindStud.Location = New System.Drawing.Point(910, 324)
+        Me.btnFindStud.Name = "btnFindStud"
+        Me.btnFindStud.Size = New System.Drawing.Size(207, 41)
+        Me.btnFindStud.TabIndex = 18
+        Me.btnFindStud.Text = "Find Student"
+        Me.btnFindStud.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1222, 752)
+        Me.Controls.Add(Me.btnFindStud)
+        Me.Controls.Add(Me.txtDOB)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtPhone)
+        Me.Controls.Add(Me.chkPaid)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.lstStud)
+        Me.Controls.Add(Me.btnAddStud)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtAvMk)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtGender)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.Name = "Form1"
+        Me.Text = "v1.8.1 Student entry form - Adding a FIND function"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtGender As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtAvMk As TextBox
+    Friend WithEvents btnAddStud As Button
+
+
+    Friend WithEvents lstStud As ListBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents chkPaid As CheckBox
+    Friend WithEvents txtPhone As MaskedTextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtDOB As DateTimePicker
+    Friend WithEvents btnFindStud As Button
+End Class
